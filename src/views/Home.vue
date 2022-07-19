098e2e85871829ed5882deb9dd21c6c8ffae208a
Kidshok <Kidshok53@gmail.com>
2022-07-19 20:14:30 +0300

    ready vue todos

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Homepage</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit sit at
+      minima nam veniam atque ipsam eius nisi sequi suscipit natus, quos facere
+      pariatur quisquam distinctio eveniet minus placeat tempore?
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
